02940026cd7987f21bac36a887d423ba2db7d8d8
Taioli Francesco <taioli.francesco98@gmail.com>
2022-02-04 17:41:27 +0100

    fix trt engine creation

--- torch_to_trt.py
+++ torch_to_trt.py
@@ -41,14 +41,16 @@ def convert_transpose(ctx):
 
     output._trt = layer.get_output(0)
 
-#@tensorrt_converter('torch.Tensor.__matmul__')
+@tensorrt_converter('torch.Tensor.__matmul__')
 def convert_mul(ctx):
     input_a = ctx.method_args[0]
     input_b = ctx.method_args[1]
     output = ctx.method_return
     input_a_trt, input_b_trt = add_missing_trt_tensors(ctx.network, [input_a, input_b])
     input_a_trt, input_b_trt = broadcast_trt_tensors(ctx.network, [input_a_trt, input_b_trt], len(output.shape) - 1)
-    layer = ctx.network.add_elementwise(input_a_trt, input_b_trt, trt.ElementWiseOperation.PROD)
+    
+    #layer = ctx.network.add_elementwise(input_a_trt, input_b_trt, trt.ElementWiseOperation.PROD)
+    layer = ctx.network.add_matrix_multiply(input_a_trt, trt.MatrixOperation.NONE, input_b_trt, trt.MatrixOperation.NONE)
     output._trt = layer.get_output(0)
 
 
